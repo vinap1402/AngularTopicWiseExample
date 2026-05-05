257d7610caed6b085e3fd9a2b2f5ffa7dcd3a20c
vinap1402 <vinap1402@gmail.com>
2026-05-05 18:14:04 +0530

    Feat:Initial project set up

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './components/nav-bar/nav-bar';
+import { Categories } from './components/categories/categories';
+import { Carousel } from './components/carousel/carousel';
+import { Body } from './components/body/body';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBar,Categories,Carousel,Body,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/body/body.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  imports: [],
+  templateUrl: './body.html',
+  styleUrl: './body.css',
+})
+export class Body {}
